02436444acafd3a944b034c862ffbba12b9b0768
Vaibhav D <vaibhav111dwivedi@gmail.com>
2022-08-18 23:15:55 +0530

    added webpacker

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '3.88.32.31', port: 22, roles: [:web, :app, :db], primary: true
+server '44.204.53.231', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,'git@github.com:dwivedivaibs/dis.git'
 set :application,     'dis'
